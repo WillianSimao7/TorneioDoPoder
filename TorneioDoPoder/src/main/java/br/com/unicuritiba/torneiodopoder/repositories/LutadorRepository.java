93440801b7d09bcfcaa4e63b6898a2e7b84af228
Willian Simão <137104998+WillianSimao7@users.noreply.github.com>
2024-04-25 21:28:34 -0300

    Delete TorneioDoPoder directory

--- TorneioDoPoder/src/main/java/br/com/unicuritiba/torneiodopoder/repositories/LutadorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.unicuritiba.torneiodopoder.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.unicuritiba.torneiodopoder.models.Lutador;
-
-public interface LutadorRepository extends
-	JpaRepository<Lutador, Long> {
-}
